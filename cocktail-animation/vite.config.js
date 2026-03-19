db6ea345dabcb05825e674bfd0562fcf4268dcd4
ckee1602-source <ckee1602@gmail.com>
2026-03-19 16:37:45 +0530

    fix: add lightningcss linux binary for tailwind v4

--- cocktail-animation/vite.config.js
+++ cocktail-animation/vite.config.js
@@ -4,5 +4,29 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss()
+  ],
+  css: {
+    transformer: 'postcss',
+    lightningcss: {
+      targets: {
+        safari: 15
+      }
+    }
+  },
+  build: {
+    cssMinify: 'lightningcss'
+  }
 })
+
+
+/*import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+})*/
